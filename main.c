37336b9eb90dbe5e66f277d8434dfb6e6bda64bf
PromptKhunanont <156408481+PromptKhunanont@users.noreply.github.com>
2024-03-01 09:56:11 +0000

    BSTBSTBST

--- main.c
+++ main.c
@@ -21,7 +21,7 @@ for ( i = 1; i < argc; ++i ) {
     inOrder(b.root);
 
    // traverse the tree postOrder
-    printf("\npostnOrder\n" ); 
+    printf("\npostOrder\n" ); 
     postOrder(b.root);  
 
 
